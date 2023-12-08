d3b7cd1a129a27b139ef451e59c71b0491555913
Kumar MM <public.kumar@gmail.com>
2023-12-07 18:45:11 -0800

    key press listener - feature complete

--- script.js
+++ script.js
@@ -21,3 +21,9 @@ for (let i = 0; i < btnOpenModals.length; i++)
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
